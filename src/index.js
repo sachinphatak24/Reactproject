744048804d09a1f1e31f2728b85de8b78f8b45bf
sachinphatak24 <phatak.sachin12@gmail.com>
2021-04-23 16:02:29 +0530

    Done

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './containers/App'
+import 'tachyons';
+import reportWebVitals from './reportWebVitals';
+
+
+ReactDOM.render(
+    <App/> 
+    ,document.getElementById('root'));
+
+    
+reportWebVitals();
